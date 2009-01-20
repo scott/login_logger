822ac4676ac8c3c99bd36270158fbfb962e12d88
Scott Miller <scott@vertster.com>
2009-01-20 16:19:05 -0700

    updated readme, added some named scopes

--- lib/login_log.rb
+++ lib/login_log.rb
@@ -1,16 +1,17 @@
 class LoginLog < ActiveRecord::Base
   belongs_to :user
   
-  
-  
-  #User last logged in
-  def self.previous
-    LoginLog.find(:last, :order => 'created_at ASC')
-  end
-  
-  #User first logged in
-  def self.first
-    LoginLog.find(:first, :order => 'created_at ASC')
-  end
+  named_scope :by_date, lambda { |*args| 
+     {
+     :conditions => ["created_at BETWEEN ? AND ?", (args[0]||Time.now).beginning_of_day.to_s(:db), (args[1]||Time.now).end_of_day.to_s(:db)] 
+     } } 
+     
+     
+  named_scope :last_ten_minutes, lambda { |*args| 
+     {
+     :conditions => ["created_at BETWEEN ? AND ?", 10.minutes.ago.to_s(:db), Time.now.to_s(:db)] 
+     } } 
+ 
+  named_scope :failed, :conditions => ['failed = ?', true]
   
 end
--- lib/login_logger.rb
+++ lib/login_logger.rb
@@ -1,20 +1,35 @@
 # LoginLogger
+#module Brainnovate
 
-
-module LoginLogger
-  
-  def log_login
-    unless current_user.blank?
-      LoginLog.create(:user_id => current_user.id,
-                      :login => current_user.login,
-                      :ip_address => request.remote_ip,
-                      :failed => false
-                      )
-    else
-      LoginLog.create(:login => params[:login],
-                      :ip_address => request.remote_ip)    
+  module LoginLogger
+    
+    def log_login
+      unless current_user.blank?
+        LoginLog.create(:user_id => current_user.id,
+                        :login => current_user.login,
+                        :ip_address => request.remote_ip,
+                        :failed => false
+                        )
+      else
+        LoginLog.create(:login => params[:login],
+                        :ip_address => request.remote_ip)    
+      end
     end
+  
+  end
+  
+  module IsLoggable
+
+    def self.included(mod)
+      mode.extend(ClassMethods)
+    end
+ 
+    module ClassMethods
+      def is_loggable
+        has_many :login_logs
+      end
+    end
+
   end
 
-  
-end
+#end
